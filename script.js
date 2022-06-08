eddcdc880eeace9f6d0e908f7c5309e1d3366ebf
Christian314 <charlygordon14@gmail.com>
2022-06-08 11:48:15 +0200

    added some comments

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let myLibrary = [];
 
+//constructor for each new book
 function Book(title, author, pages, read) {
         this.title = title;
         this.author = author;
@@ -7,11 +8,13 @@ function Book(title, author, pages, read) {
         this.read = read;
 }
 
+//adds book to Library
 function addBookToLibrary(title, author, pages, read) {
   let newBook = new Book(title, author, pages, read);  
   myLibrary.push(newBook);
 }
 
+//just some dummy books
 addBookToLibrary("HP","JKR","300","Read")
 addBookToLibrary("HPA","JKR","300","Read")
 addBookToLibrary("HPB","JKR","300","Read")
@@ -19,3 +22,16 @@ addBookToLibrary("HPC","JKR","300","Read")
 addBookToLibrary("HPD","JKR","300","Read")
 addBookToLibrary("HPE","JKR","300","Read")
 addBookToLibrary("HPF","JKR","300","Read")
+
+//renders the information for each book on screen
+function renderBook(book) {
+  
+}
+
+//loops through library and calls renderBook() for each book
+function createLibrary(myLibrary) {
+  for (const book of myLibrary) {
+    renderBook(book);
+  }
+}
+
